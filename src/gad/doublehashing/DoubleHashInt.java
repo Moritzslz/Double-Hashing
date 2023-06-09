cadc0f366933b6e4b8f35f35e293d5c753d3c7b5
moritzslz <schultzmoritz@gmail.com>
2023-06-09 18:55:14 +0200

    HashInt Test

--- src/gad/doublehashing/DoubleHashInt.java
+++ src/gad/doublehashing/DoubleHashInt.java
@@ -8,7 +8,6 @@ public class DoubleHashInt implements DoubleHashable<Integer> {
 	int[] vector;
 	public DoubleHashInt(int primeSize) {
 		this.primeSize = primeSize;
-		this.vector = new int[]{1,2,3,4,5,6,7,8,9};
 	}
 
 	@Override
@@ -18,14 +17,6 @@ public class DoubleHashInt implements DoubleHashable<Integer> {
 
 	@Override
 	public int hashTick(Integer key) {
-		byte[] bytes = bytes(key);
-		int sum = 0;
-		for (int i = 0; i < bytes.length; i++) {
-			sum += bytes[i] * vector[i % vector.length];
-		}
-		return sum % primeSize;
-	}
-	private byte[] bytes(Integer key) {
-		return key.toString().getBytes(StandardCharsets.UTF_8);
+		return 1 + (key/primeSize) % (primeSize - 1);
 	}
 }
