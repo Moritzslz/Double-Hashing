83eaad5e8f8cc5adc9f991979800ad5ee0159292
moritzslz <schultzmoritz@gmail.com>
2023-06-09 16:14:48 +0200

    DoubleHashInt % DoubleHashString

--- src/gad/doublehashing/DoubleHashInt.java
+++ src/gad/doublehashing/DoubleHashInt.java
@@ -1,17 +1,44 @@
 package gad.doublehashing;
 
+import java.nio.charset.StandardCharsets;
+
 public class DoubleHashInt implements DoubleHashable<Integer> {
 
+	int primeSize;
+	int[] a;
+	int[] b;
+
 	public DoubleHashInt(int primeSize) {
+		this.primeSize = primeSize;
+		a = new int[] {1, 2, 3};
+		b= new int[] {4, 5, 6};
 	}
 
 	@Override
 	public int hash(Integer key) {
-		return 0;
+		byte[] x = bytes(key);
+		int sum = 0;
+		for (int i = 0; i < x.length; i++) {
+			sum += x[i] * a[i % a.length];
+		}
+		return fastModulo(sum, primeSize);
 	}
 
 	@Override
 	public int hashTick(Integer key) {
-		return 0;
+		byte[] x = bytes(key);
+		int sum = 0;
+		for (int i = 0; i < x.length; i++) {
+			sum += x[i] * b[i % b.length];
+		}
+		return fastModulo(sum, primeSize);
+	}
+
+	private byte[] bytes(Integer key) {
+		return key.toString().getBytes(StandardCharsets.UTF_8);
+	}
+
+	public static int fastModulo(int i, int divisor) {
+		return i & (divisor - 1);
 	}
 }
--- src/gad/doublehashing/DoubleHashString.java
+++ src/gad/doublehashing/DoubleHashString.java
@@ -1,17 +1,43 @@
 package gad.doublehashing;
 
+import java.nio.charset.StandardCharsets;
+
 public class DoubleHashString implements DoubleHashable<String> {
 
+	int primeSize;
+	int[] a;
+	int[] b;
 	public DoubleHashString(int primeSize) {
+		this.primeSize = primeSize;
+		a = new int[] {1, 2, 3};
+		b= new int[] {4, 5, 6};
 	}
 
 	@Override
 	public int hash(String key) {
-		return 0;
+		byte[] x = bytes(key);
+		int sum = 0;
+		for (int i = 0; i < x.length; i++) {
+			sum += x[i] * a[i % a.length];
+		}
+		return fastModulo(sum, primeSize);
 	}
 
 	@Override
 	public int hashTick(String key) {
-		return 0;
+		byte[] x = bytes(key);
+		int sum = 0;
+		for (int i = 0; i < x.length; i++) {
+			sum += x[i] * b[i % b.length];
+		}
+		return fastModulo(sum, primeSize);
+	}
+
+	private byte[] bytes(String key) {
+		return key.getBytes(StandardCharsets.UTF_8);
+	}
+
+	public static int fastModulo(int i, int divisor) {
+		return i & (divisor - 1);
 	}
 }
