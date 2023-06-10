2d6afd25514150fedceb2c1aea56b5f66ac19b08
moritzslz <schultzmoritz@gmail.com>
2023-06-10 13:48:13 +0200

    try

--- src/gad/doublehashing/DoubleHashTable.java
+++ src/gad/doublehashing/DoubleHashTable.java
@@ -26,71 +26,31 @@ public class DoubleHashTable<K, V> {
 	}
 
 	public boolean insert(K k, V v) {
-		int hash0 = hash(k, 0);
-		int hash1 = hash(k, 1);
-		int hash2 = hash(k, 2);
-		int hash3 = hash(k, 3);
-		if (pairs[hash0] == null) {
-			//Initialize
-			pairs[hash0] = new Pair<>(k, v);
-			return true;
-		} else if (pairs[hash0].one() == k) {
-			//Overwrite
-			pairs[hash0] = new Pair<>(k, v);
-			return true;
-		} else if (pairs[hash1].one() == k) {
-			//Skip
-			pairs[hash1] = new Pair<>(k, v);
+		int index = hash(k, 0);
+		int rehashes = 0;
+
+		while (pairs[index] != null && !pairs[index].one().equals(k)) {
+			index = hash(k, ++rehashes);
 			collisions++;
-			if (maxRehashes < 1)
-				maxRehashes = 1;
-			return true;
-		} else if (pairs[hash2].one() == k) {
-			//Skip
-			pairs[hash2] = new Pair<>(k, v);
-			collisions++;
-			if (maxRehashes < 2)
-				maxRehashes = 2;
-			return true;
-		} else if (pairs[hash3].one() == k) {
-			//Skip
-			pairs[hash3] = new Pair<>(k, v);
-			collisions++;
-			if (maxRehashes < 3)
-				maxRehashes = 3;
-			return true;
 		}
-		return false;
+
+		pairs[index] = new Pair<>(k, v);
+		maxRehashes = Math.max(maxRehashes, rehashes);
+		return true;
 	}
 
 	public Optional<V> find(K k) {
-		int hash0 = hash(k, 0);
-		int hash1 = hash(k, 1);
-		int hash2 = hash(k, 2);
-		int hash3 = hash(k, 3);
-		if (pairs[hash0] == null) {
-			return Optional.empty();
-		} else if (pairs[hash0].one() == k) {
-			if (pairs[hash0].two() != null)
-				return Optional.of(pairs[hash0].two());
-			else
-				return Optional.empty();
-		} else if (pairs[hash1].one() == k) {
-			if (pairs[hash1].two() != null)
-				return Optional.of(pairs[hash1].two());
-			else
-				return Optional.empty();
-		} else if (pairs[hash2].one() == k) {
-			if (pairs[hash2].two() != null)
-				return Optional.of(pairs[hash2].two());
-			else
-				return Optional.empty();
-		} else if (pairs[hash3].one() == k) {
-			if (pairs[hash3].two() != null)
-				return Optional.of(pairs[hash3].two());
-			else
-				return Optional.empty();
+		int index = hash(k, 0);
+		int rehashes = 0;
+
+		while (pairs[index] != null) {
+			if (pairs[index].one().equals(k)) {
+				return Optional.of(pairs[index].two());
+			}
+
+			index = hash(k, ++rehashes);
 		}
+
 		return Optional.empty();
 	}
 
